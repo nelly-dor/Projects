db4ca600b73b7e5746fc2c54f315ee6f9eb2bd23
N D <example@examplemail.com>
2025-04-21 12:35:43 +0200

    Read and write high scores to 'data.txt'

--- Python/Snake/scoreboard.py
+++ Python/Snake/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt", mode="r") as hs:
+            self.high_score = int(hs.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as hs:
+                hs.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
